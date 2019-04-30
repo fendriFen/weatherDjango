920ce3cb8d76494d6fc365d890be98339b4904d4
fendriFen <38529308+fendriFen@users.noreply.github.com>
2019-04-30 11:43:41 +0200

    Update conf.sh

--- the_weather/conf.sh
+++ the_weather/conf.sh
@@ -52,4 +52,4 @@ sudo sed -i "s/SECRET_KEY =.*/SECRET_KEY = config\['SECRET_KEY'\] /" settings.py
 pwd
 cd ~/weatherDjango/the_weather/
 echo "done"
-source venv/bin/activate | python3 manage.py runserver 0.0.0.0:8000 &
+source venv/bin/activate | python3 manage.py runserver 0.0.0.0:8000 & ;
