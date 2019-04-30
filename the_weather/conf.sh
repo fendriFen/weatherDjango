7d6012fc2a74dd8f64b9eabc7a5b29692f52ac54
fendriFen <38529308+fendriFen@users.noreply.github.com>
2019-04-30 11:13:15 +0200

    Update conf.sh

--- the_weather/conf.sh
+++ the_weather/conf.sh
@@ -13,29 +13,43 @@ yes | sudo ufw enable
 sudo ufw status
 sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-venv
-cd weatherDjango
-ls
+echo "--------------------"
+pwd
+cd ~/weatherDjango
+pwd
 python3 -m venv venv 
-source venv/bin/activate
+cd ~/weatherDjango/the_weather | source venv/bin/activate &
 cd ./the_weather
 sudo apt-get install mysql-server 
 pip3 install -r requirements.txt
-sed -i 's/DEBUG = True/DEBUG = False/' weatherDjango/the_weather/the_weather/settings.py
-sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[$adrss\]/' weatherDjango/the_weather/the_weather/settings.py
+echo "--------------------"
+echo "____________________"
+cd ./the_weather
+pwd
+sudo sed -i 's/DEBUG = True/DEBUG = False/' settings.py
+echo "false ---> true done"
+echo "____________________"
+x=\"
+adr=$x$1$x
+
+echo $adr
+echo $adrss
+echo $1
+sudo sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = [$adr]/"  settings.py
 sudo touch /etc/config.json 
 sudo chown ubuntu /etc/config.json
-sed -n '23p' weatherDjango/the_weather/the_weather/settings.py > /etc/config.json
+echo "config.json created"
+sudo sed -n '23p' settings.py > /etc/config.json
 sudo sed -i 's/.*=/ /' /etc/config.json
-sudo sed -i "s/\'/+/g" /etc/config.json
-sudo sed -i "s/+/\"/g" /etc/config.json
-sudo sed -i 's/^/\t\"SECRET_KEY: /' /etc/config.json
+sudo sed -i "s/'/+/g" /etc/config.json
+sudo sed -i 's/+/"/g' /etc/config.json
+sudo sed -i 's/^/\t\"SECRET_KEY": /' /etc/config.json
 sudo sed -i 's/^/\{\n /' /etc/config.json
 sudo chown ubuntu /etc/config.json
-echo "}" >> /etc/config.json
-sed -i "s/import os/import os \nimport json \nwith open (\'\/etc\/config.json\') as config_file: \n\tconfig = json.load\(config_file\) /g" weatherDjango/the_weather/the_weather/settings.py
-sed -i 's/=.*/ = config\['SECRET_KEY'\] /' weatherDjango/the_weather/the_weather/settings.py
-cd weatherDjango/the_weather/
-python3 manage.py runserver 0.0.0.0:8000 &
-
-
-
+sudo echo "}" >> /etc/config.json
+sudo sed -i "s/import os/import os \nimport json \nwith open (\'\/etc\/config.json\') as config_file: \n\tconfig = json.load\(config_file\) /g" settings.py
+sudo sed -i "s/SECRET_KEY =.*/SECRET_KEY = config\['SECRET_KEY'\] /" settings.py
+pwd
+cd ~/weatherDjango/the_weather/
+echo "done"
+source venv/bin/activate | python3 manage.py runserver 0.0.0.0:8000 &
