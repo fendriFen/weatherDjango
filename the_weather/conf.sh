6d1ca908610e926b8931168082bd1367f99bf552
fendriFen <38529308+fendriFen@users.noreply.github.com>
2019-05-01 17:22:40 +0200

    Update conf.sh

--- the_weather/conf.sh
+++ the_weather/conf.sh
@@ -53,6 +53,6 @@ pwd
 cd ~/weatherDjango/the_weather/
 echo "done"
 #source venv/bin/activate | echo | 
-commande='python3 manage.py runserver 0.0.0.0:8000' 
-$commande &
+python3 manage.py runserver 0.0.0.0:8000 &>/dev/null 
+
 
